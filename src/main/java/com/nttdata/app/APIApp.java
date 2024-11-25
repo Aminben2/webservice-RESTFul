de5cf77c7db71ffd5b4114aa3bc9c1fb64869884
Aminben2 <aminbenomar454@gmail.com>
2024-11-25 09:49:35 +0100

    done :)

--- src/main/java/com/nttdata/app/APIApp.java
+++ src/main/java/com/nttdata/app/APIApp.java
@@ -1,11 +1,11 @@
 package com.nttdata.app;
 
 
+import com.nttdata.app.config.CorsFilter;
 import com.nttdata.app.webservice.person.PersonWebServiceImpl;
 import com.nttdata.app.webservice.user.UserWebService;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
-
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,5 +20,15 @@ public class APIApp extends Application {
     @Override
     public Set<Object> getSingletons() {
         return singletons;
+    }
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet<>();
+        // Register your resources
+        resources.add(UserWebService.class);
+        // Register the CORS filter
+        resources.add(CorsFilter.class);
+
+        return resources;
     }
 }
